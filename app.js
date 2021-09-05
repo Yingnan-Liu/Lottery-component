b9e33d2a5ff3ee47ab8aaceb640087582bf11d7d
Lynn <lynn.yingnanliu@gmail.com>
2021-09-05 14:47:57 +0800

    add cors middleware

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 
 app.use(express.static("public"));
-
+app.use(cors());
 app.get("/testPage", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
